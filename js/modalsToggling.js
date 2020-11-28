121e6f0fd7cce152e0edd8deeb005178fb857276
Anthony Guerrero <adguerrero1935@gmail.com>
2020-11-27 20:30:35 -0500

    Creating a modal for the code quiz

--- js/modalsToggling.js
+++ js/modalsToggling.js
@@ -2,9 +2,18 @@ const toggleModal = () => {
     document.querySelector(".modalForWeather").classList.toggle("hideWeatherModal")
 };
 
+const toggleCodeModal = () => {
+    document.querySelector(".modalForCodeQuiz").classList.toggle("hideCodeQuizModal")
+
+
+};
+
 
 $(".weatherBtn").on("click", toggleModal);
 $(".modalWeatherCloseBtn").on("click", toggleModal);
+
+$(".codeBtn").on("click", toggleCodeModal);
+$(".modalCodeQuizCloseBtn").on("click" , toggleCodeModal);
 
 // document.querySelector(".weatherBtn").addEventListener("click", toggleModal);
 // document.querySelector(".modalWeatherCloseBtn").addEventListener("click", toggleModal);
