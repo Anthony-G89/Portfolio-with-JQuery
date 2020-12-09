a11220f6ea5ef37d46faffd4185f2eb78acf0550
Anthony Guerrero <adguerrero1935@gmail.com>
2020-12-08 23:11:15 -0500

    added a arrow button for when clicked on it will scroll back to the top of the page

---
+++ js/topBtnFunction.js
@@ -0,0 +1,19 @@
+topBtn = $(".topBtn");
+
+// Testing to see how many pixles am I scrolling
+window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+    // console.log(scrolled);
+});
+
+
+// triggering button to appear when reaching a certain amount of pixels
+window.addEventListener("scroll", ()=> {
+    const scrolled = document.querySelector(".topBtn");
+    scrolled.classList.toggle("active", window.scrollY > 2210)
+});
+
+// when clicked on button will automatically scroll to the top of the page
+topBtn.on("click", () => {
+    $("html, body").animate({scrollTop: 0}, "slow")
+});
