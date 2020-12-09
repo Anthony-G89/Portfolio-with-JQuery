95ddf65652ac6cfbe3308c5ed30a7006348949c7
Anthony Guerrero <adguerrero1935@gmail.com>
2020-12-08 23:36:03 -0500

    Added an animation for how the modals pop up

--- js/topBtnFunction.js
+++ js/topBtnFunction.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () => {
 // triggering button to appear when reaching a certain amount of pixels
 window.addEventListener("scroll", ()=> {
     const scrolled = document.querySelector(".topBtn");
-    scrolled.classList.toggle("active", window.scrollY > 2210)
+    scrolled.classList.toggle("active", window.scrollY > 1899)
 });
 
 // when clicked on button will automatically scroll to the top of the page
