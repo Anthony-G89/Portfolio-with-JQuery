15eaec17f81996f58fd7b31f02b28245e0c1487e
Anthony Guerrero <adguerrero1935@gmail.com>
2021-01-14 15:17:33 -0500

    fixing navBar menu for mobile being used horizontal or vartical

--- js/menu-functions.js
+++ js/menu-functions.js
@@ -1,7 +1,27 @@
 const menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
+const body = document.querySelector(".body");
+// const navLink =document.querySelector(".nav-link");
 
 menu.addEventListener("click", () =>{
     // console.log("clicked on")
     navbar.classList.toggle("change");
 });
+
+// navLink.addEventListener('click', () =>{
+//         navbar.style.display = "hidden" 
+// });
+
+// body.addEventListener('click', () => {
+//     navbar.style.display = "";
+//     // console.log("? on");
+// })
+
+
+// function outSideClick (e) {
+//     if(e.target == body) {
+//         navbar.style.display = "none"
+//     }
+// }
+
+// window.addEventListener("click", outSideClick)
